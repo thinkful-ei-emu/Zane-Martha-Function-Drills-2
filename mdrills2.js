d7138c2d851d3ad6dfe3e128d88dfa69260c33af
Martha Plantz <maplantz89@gmail.com>
2019-06-03 15:31:40 -0700

    finished the decoding function

--- mdrills2.js
+++ mdrills2.js
@@ -6,6 +6,8 @@ function jediName(firstName, lastName){
   let jname = jfirst + jlast;
   return jname;
 }
+const jName = jediName('Martha', 'Plantz');
+console.log(jName);
 
 function beyond(num){
   if (isFinite(num) === false && typeof(num)==='number'){
@@ -19,5 +21,32 @@ function beyond(num){
   }
 }
 
-const jName = jediName('Martha', 'Plantz');
-console.log(jName);
+function decode (word){
+  let answer = [];
+  let allWords = word.split(' ');
+  allWords.forEach(word => { 
+    let firstLetter = word[0];
+    switch (firstLetter) {
+    case 'a':
+    //return second letter
+      answer.push(word[1]);
+      break;
+    case 'b':
+      //return 3rd letter
+      answer.push(word[2]);
+      break;
+    case 'c':
+    //return 4th letter
+      answer.push(word[3]);
+      break;
+    case 'd':
+      //return 5th letter
+      answer.push(word[4]);
+      break;
+    default:
+      answer.push(' ');
+    }
+  });
+}
+const code = decode('craft block argon meter bells brown croon droop');
+console.log(code);
